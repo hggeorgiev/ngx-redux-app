99b773af9e7550c271662c10c360f08da601c88e
Kaizeras <hgeorgievkzr@gmail.com>
2016-11-08 02:29:49 +0200

    finishing part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
+import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {StoreModule} from "@ngrx/store";
+import {operationsReducer} from "./common/operations";
+import {CommonModule} from "@angular/common";
+import {NewOperation} from "./new-operation.component";
+import {OperationsList} from "./operations-list.component";
+
 
 @NgModule({
+  bootstrap: [ AppComponent ],
   declarations: [
-    AppComponent
+    AppComponent,
+    NewOperation,
+    OperationsList,
   ],
-  imports: [
+  imports: [ // import Angular's modules
     BrowserModule,
+    CommonModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore({ operations: operationsReducer }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {}
+
+
+
+}
+
